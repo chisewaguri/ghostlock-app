0813ed30bef46bf814228c6d1b175ddbb9491cdc
YuKongA <70465933+YuKongA@users.noreply.github.com>
2026-08-09 19:58:09 +0800

    Add OPPO Find X9 support (#12)

---
+++ src/kernels/6.12.23-android16-5-g82efd98459a2-ab14457512-4k/offsets.h
@@ -0,0 +1,38 @@
+/* 6.12.23-android16-5-g82efd98459a2-ab14457512-4k */
+
+OFFSETS_ENTRY(
+    "6.12.23-android16-5-g82efd98459a2-ab14457512-4k",
+    STRUCT_OFFSETS_6_12,
+    .kernel_phys_load = 0x80000000,
+    .pselect_waiter_shift = 0,
+    .off_init_task = 0x023ecf00,
+    .off_init_cred = 0x02402a68,
+    .off_root_task_group = 0x0261c580,
+    .off_selinux_enforcing = 0x026684f0,
+    .off_selinux_blob_sizes = 0x0182f4e8,
+    .off_security_hook_heads = 0x00000000,
+    .off_kmalloc_caches = 0x018294c0,
+    .off_anon_pipe_buf_ops = 0x0120ed88,
+    .off_slide_nfulnl_logger = 0x023e2198,
+    .off_slide_boot_id = 0x02689868,
+    .off_configfs_read_iter = 0x00511a7c,
+    .off_configfs_bin_write_iter = 0x00512028,
+    .off_copy_splice_read = 0x0048e034,
+    .off_noop_llseek = 0x0043b678,
+    .off_slide_loggers_0_1 = 0x023e20e8,
+    .off_ashmem_fops = 0x02695a88,
+    .off_ashmem_misc_fops = 0x0132ae78,
+    .off_ashmem_ioctl = 0x00d7e5a8,
+    .off_ashmem_compat_ioctl = 0x00d7eb90,
+    .off_ashmem_mmap = 0x00d7ec0c,
+    .off_ashmem_open = 0x00d7ec68,
+    .off_ashmem_release = 0x00d7e668,
+    .off_ashmem_show_fdinfo = 0x00d7eb68,
+),
+
+/* BTF reference (runtime uses target.h defaults): */
+/* #define STRUCT_PAGE_SIZE 0x40 */
+/* #define STRUCT_PAGE_COMPOUND_HEAD 0x8 */
+/* #define STRUCT_PAGE_TYPE 0x30 */
+/* #define STRUCT_SLAB_CACHE 0x8 */
+/* #define STRUCT_MM_STRUCT 0x4C0 */
