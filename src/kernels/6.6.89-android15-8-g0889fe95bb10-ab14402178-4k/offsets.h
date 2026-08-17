adf8c18e136e637e73b134812a25d6df077add07
fahrulseptiana <10472373+fahrulseptiana@users.noreply.github.com>
2026-08-17 14:01:41 +0700

    Add dash (POCO X8 Pro Max) with 6.6.89 kernel (#35)

---
+++ src/kernels/6.6.89-android15-8-g0889fe95bb10-ab14402178-4k/offsets.h
@@ -0,0 +1,23 @@
+/* 6.6.89-android15-8-g0889fe95bb10-ab14402178-4k */
+
+OFFSETS_ENTRY(
+    "6.6.89-android15-8-g0889fe95bb10-ab14402178-4k",
+    STRUCT_OFFSETS_6_6,
+    .pselect_waiter_shift = -2,
+    .off_init_task = 0x020fe280,
+    .off_init_cred = 0x02110548,
+    .off_root_task_group = 0x022f5580,
+    .off_selinux_enforcing = 0x02336ea0,
+    .off_selinux_blob_sizes = 0x016625f0,
+    .off_security_hook_heads = 0x01661eb8,
+    .off_slide_nfulnl_logger = 0x020f2258,
+    .off_slide_boot_id = 0x02357e98,
+    .off_slide_loggers_0_1 = 0x020f21a8,
+),
+
+/* BTF reference (runtime uses target.h defaults): */
+/* #define STRUCT_PAGE_SIZE 0x40 */
+/* #define STRUCT_PAGE_COMPOUND_HEAD 0x8 */
+/* #define STRUCT_PAGE_TYPE 0x30 */
+/* #define STRUCT_SLAB_CACHE 0x8 */
+/* #define STRUCT_MM_STRUCT 0x4C0 */
